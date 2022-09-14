46dfdbc11e9ee62b5837ab29fb2e46cbe53e7ad1
paulovianna <paulo2001.vianna@gmail.com>
2022-09-14 14:39:11 -0300

    Projeto de conta. Com Styled Components

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
-
+import { Icone, Box, Botao, Detalhe, Saldo, IconeTema } from "../UI";
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
+import styled from "styled-components";
+
+
+const IconeMargin =styled(Icone)`
+  margin-top: 2px;
+`
 
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
@@ -11,30 +17,28 @@ const Conta = () => {
     untoggle((toggleState) => !toggleState);
   };
 
-  return (
-    <div className="box">
+  return (  
+    <Box>
       <h2>Conta</h2>
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponível{" "}
         <span>
-          <img className="imagem-icone" src={dinheiro} alt="Ícone Saldo" />
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
         </span>
         {toggleState ? (
-          <div className="saldo">
-            <span className="detalhe">R$</span> 0,00{" "}
-          </div>
+          <Saldo>
+            <Detalhe>R$</Detalhe> 578,00{" "}
+          </Saldo>
         ) : null}
       </div>
 
-      <button className="btn" onClick={toggleHandler}>
-        <img
-          style={{ marginTop: "2px" }}
-          className="imagem-icone"
+      <Botao onClick={toggleHandler}>
+        <IconeMargin /* Herança de estilo de Icone */
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
-      </button>
-    </div>
+      </Botao>
+    </Box>
   );
 };
 
---
+++ src/Components/UI/variaveis.js
@@ -0,0 +1,9 @@
+export const corPrimaria = "#41d3be";
+
+export const fundoClaro = "#f1f1f1";
+export const conteudoClaro = "white";
+export const textofundoClaro = "grey";
+
+export const fundoEscuro = "#1C1C1C";
+export const conteudoEscuro = "#5c5b5e";
+export const textoFundoEscuro = "#fafafa";
